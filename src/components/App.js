54a6c8c07d66e67dd793e611ef69a54be1f57ef6
dishant <dishantgandhi99@yahoo.com>
2020-10-01 14:27:16 +0530

    Adding Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,48 @@ class App extends Component {
 
     return (
       <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img src="" at="logo"></img>
+          </div>
+          <div className="search-container">
+            <input placeholder="Search" alt="search-icon" />
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://www.flaticon.com/svg/static/icons/svg/3532/3532842.svg"
+                    alt="user-dp"
+                  />
+                  <span>JohnDoe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://www.flaticon.com/svg/static/icons/svg/3532/3532842.svg"
+                    alt="user-dp"
+                  />
+                  <span>JohnDoe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <li className="search-results-row">
+              <img
+                src="https://www.flaticon.com/svg/static/icons/svg/3532/3532842.svg"
+                alt="user-dp"
+              />
+              <span>JohnDoe</span>
+            </li>
+          </div>
+          <div className="nav-links">
+            <ul>
+              <li>Log in</li>
+              <li>Log Out</li>
+              <li>Register</li>
+            </ul>
+          </div>
+        </nav>
         <PostList posts={posts} />
       </div>
     );
